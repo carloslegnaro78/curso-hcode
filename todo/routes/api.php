5f71ce7ca7ee2c88f74e5912c5e1f88e7c8f49d3
Carlos Legnaro <carlos_legnaro@outlook.com>
2020-08-15 15:46:34 -0300

    Instalando o Postman

--- todo/routes/api.php
+++ todo/routes/api.php
@@ -4,6 +4,10 @@ use Illuminate\Http\Request;
 
 // http://127.0.0.1:8000/api/tasks
 
+//Visual Studio Code PHP Intelephense Keep Showing Not Necessary Error
+
+//https://stackoverflow.com/questions/59149877/visual-studio-code-php-intelephense-keep-showing-not-necessary-error
+
 Route::get('tasks', function(){
 
     $tasks = [
